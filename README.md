6227cb79fc0f2170771f5551ca6efb1d3477fac8
Jason Pitsch <jason.pitsch@campingworld.com>
2021-01-20 10:09:39 -0600

    Initial commit, working example, readme and package.json

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # p2p-cdn-image-worker
+
+This is a demo project on how to hook up the workers to S3 in order to serve images.  This is currently set up to hook into AWS but the URL structure can easily be changed to support whatever service is being used.
+
+## Usage
+
+Example Request: `images.p2p-images.workers.dev/1234/image.jpg`
+
+The script is currently set up to use the listings ID along with the assests name.  This is under the assumption that this is how the images are stored.
+
+## How to Deploy
+
+//TODO - Working on setting up an example of using Github Actions to deploy when there is a merge into the `main` branch.
---
+++ index.js
@@ -0,0 +1,34 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event))
+})
+
+// This would be whatever you have setup in AWS.
+const BUCKET_NAME = 'somebucketname'
+const REGION = 'sample-region-x'
+const BUCKET_URL = `https://s3.${REGION}.amazonaws.com/${BUCKET_NAME}`
+
+async function servePicture(event) {
+  const url = new URL(event.request.url)
+  const cache = caches.default
+  let response = await cache.match(event.request)
+
+  if(!response) { 
+    response = await fetch(`${BUCKET_URL}${url.pathname}`)
+    const headers = { "cache-control": "public, max-age=14400" }
+    response = new Response(response.body, { ...response, headers })
+    event.waitUntil(cache.put(event.request, response.clone()))
+  }
+  return response
+}
+
+async function handleRequest(event) {
+  if(event.request.method === 'GET') {
+    let response = await servePicture(event)
+    if(response.status > 399) { 
+      response = new Response(response.statusText, { status: response.status })
+    }
+    return response
+  } else {
+    return new Response('Invalid Request Type', { status: 405 })
+  }
+}
